12cff99077953e06dcbca70fd7b1c97b09a78cfc
Sem8 <mssemmi8@gmail.com>
2019-04-01 18:47:37 -0400

    MVP I think

--- routers/login-router.js
+++ routers/login-router.js
@@ -2,8 +2,43 @@ const loginRouter = require('express').Router();
 const knex = require('knex');
 const bcrypt = require('bcryptjs');
 
-loginRouter.get('/', (req, res) => {
-    res.send('Please log in');
+const userdb = require('../database/dbConfig.js');
+const Users = require('../users/users-model.js');
+
+loginRouter.post('/', (req, res) => {
+    let { username, password } = req.body;
+    Users.findBy({ username }).first()
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            res.status(200).json({ message: `Welcome ${user.username} you're logged in.`});
+        } else {
+            res.status(401).json({ message: 'You shall not pass, your credentials are invalid'});
+        }
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    });
+
+    // res.send('Please log in');
 });
+
+// server.post('/api/login', (req, res) => {
+//     let { username, password } = req.body;
+//     users('users')
+//       .where({ username })
+//       .first()
+//       .then(user => {
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//           res.status(200).json({ message: `Hello ${user.username}` });
+//         } else {
+//           res
+//             .status(401)
+//             .json({ message: 'Sorry, we could not find you in our systems' });
+//         }
+//       })
+//       .catch(error => {
+//         res.status(500).json(error);
+//       });
+//   });
 
 module.exports = loginRouter;
