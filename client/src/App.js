0057d7e56720c6ae39dd6f6d18b100505b502c45
Sem8 <mssemmi8@gmail.com>
2019-04-03 01:28:00 -0400

    changes to the ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,88 @@
 import React, { Component } from "react";
+import { Link, withRouter, BrowserRouter as Router } from "react-router-dom";
+
 import "./App.css";
 
 class App extends Component {
-  state = { users: [] };
+  state = {
+    isLoading: true,
+    token: "",
+    signUpError: "",
+    signInError: "",
+    signInEmail: "",
+    signInPassword: "",
+    signUpEmail: "",
+    signUpPassword: "",
+    users: []
+  };
 
   componentDidMount() {
-    fetch("http://localhost:5000/api/users")
+    const token = localStorage.getItem(this.state.token);
+    if (token) {
+      fetch("/api/login" + token)
+        .then(res => res.json())
+        .then(json => {
+          if (json.success) {
+            this.setState({
+              token,
+              isLoading: false
+            });
+          } else {
+            this.setState({
+              isLoading: false
+            });
+          }
+        });
+    }
+    fetch("/api/users")
       .then(res => res.json())
       .then(users => this.setState({ users }));
   }
 
+  logOut(e) {
+    e.preventDefault();
+    localStorage.removeItem("usertoken");
+    this.props.history.push("/");
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
+    };
+
+    if (!this.state.token) {
+      return (
+        <div>
+          <p>Sign In</p>
+          <p>Sign up</p>
+        </div>
+      );
+    };
+
     return (
-      <div className="App">
-        <h1>Users</h1>
-        <ul>
-          {this.state.users.map(user => (
-            <li key={user.id}>{user.username}</li>
-          ))}
-        </ul>
-      </div>
+      <Router>
+        <div className="App">
+          <h1>Users</h1>
+          <ul>
+            {this.state.users.map(user => (
+              <li key={user.id}>{user.username}</li>
+            ))}
+          </ul>
+          <div>
+            <Link to="/api/register">Register</Link>
+          </div>
+
+          <a href="" onClick={this.logOut}>
+            Logout
+          </a>
+          <Link to="/">Home</Link>
+          {localStorage.token ? (
+            <Link to="/api/users">User</Link>
+          ) : (
+            <Link to="/api/login">Login</Link>
+          )}
+        </div>
+      </Router>
     );
   }
 }
